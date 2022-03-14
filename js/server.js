f8418b0a2f50534d431206b11a2832bc749dc3a3
renedonaire <rene.donaire@gmail.com>
2022-03-13 23:35:18 -0300

    Agrega html

---
+++ js/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const routerProductos = require('../router/routerProductos')
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(express.json())
+
+app.use('/api/productos', routerProductos)
+
+const PORT = 8080
+const server = app.listen(PORT, () => {
+	console.log(`Servidor escuchando el puerto ${server.address().port}`)
+})
+
+server.on('error', (error) => console.log(`Error en servidor ${error}`))
