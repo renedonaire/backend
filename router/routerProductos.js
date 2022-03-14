dcd03ff30e75bcbb2432e43438d72202dde4775c
renedonaire <rene.donaire@gmail.com>
2022-03-14 00:46:23 -0300

    TODO: migrar api a clase

--- router/routerProductos.js
+++ router/routerProductos.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { Router } = require('express')
 const routerProductos = Router()
+const fs = require('fs')
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
