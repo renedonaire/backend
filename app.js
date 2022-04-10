686634cd438491e25fb70428fa9d5d601d613724
renedonaire <rene.donaire@gmail.com>
2022-04-10 18:38:00 -0400

    Transforma 'productos' en una clase

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const routerProductos = require('./router/routerProductos')
 const { Server: HTTPServer } = require('http')
 const { Server: SocketServer } = require('socket.io')
-const { getProducts, saveProduct } = require('./models/productos')
+const { Router } = require('express')
+const routerProductos = Router()
 
 const app = express()
 const httpServer = new HTTPServer(app)
@@ -12,6 +12,7 @@ const io = new SocketServer(httpServer)
 const path = require('path')
 const route = path.join(__dirname, './storage/mensajes.txt')
 const Mensajes = require('./models/mensajes')
+const Productos = require('./models/productos')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
@@ -27,13 +28,20 @@ app.engine(
 app.use('/', routerProductos)
 
 const mensajes = new Mensajes(route)
+const productos = new Productos()
+const arrayProductos = JSON.stringify(productos.getProducts())
+
+routerProductos.get('/', (req, res) => {
+	res.render('../views/layouts/main.hbs', { list: arrayProductos })
+})
+
 io.on('connection', async (socket) => {
 	console.log('Nuevo cliente conectado')
 
 	const messages = await mensajes.getMessages()
 	socket.emit('messages', messages)
 
-	const products = getProducts()
+	const products = productos.getProducts()
 	socket.emit('products', products)
 
 	socket.on('new-message', async (message) => {
@@ -43,8 +51,8 @@ io.on('connection', async (socket) => {
 	})
 
 	socket.on('new-product', (product) => {
-		saveProduct(product)
-		const allProducts = getProducts()
+		productos.saveProduct(product)
+		const allProducts = productos.getProducts()
 		io.sockets.emit('products', allProducts)
 	})
 })
