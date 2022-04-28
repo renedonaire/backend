2f0d071d27051c18a62a7af51b804cf75d17eb30
renedonaire <rene.donaire@gmail.com>
2022-04-28 18:34:00 -0400

    Agrega routers

---
+++ src/routers/carritosRouter.js
@@ -0,0 +1,31 @@
+import { Router } from 'express'
+import { carritosDao } from '../daos/daos.js'
+
+const carritosRouter = Router()
+
+carritosRouter.get('/', async (req, res) => {
+	const carritos = await carritosDao.listarAll()
+	res.json(carritos)
+})
+
+carritosRouter.get('/:id', async (req, res) => {
+	const carritos = await carritosDao.listar(req.params.id)
+	res.json(carritos)
+})
+
+carritosRouter.post('/', async (req, res) => {
+	const carroAgregado = await carritosDao.guardar(req.body)
+	res.json(carroAgregado)
+})
+
+carritosRouter.put('/:id', async (req, res) => {
+	const carroActualizado = await carritosDao.actualizar(req.body)
+	res.json(carroActualizado)
+})
+
+carritosRouter.delete('/:id', async (req, res) => {
+	const carroEliminado = await carritosDao.borrar(req.params.id)
+	res.json(carroEliminado)
+})
+
+export { carritosRouter }
