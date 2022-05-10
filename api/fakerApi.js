1285b0aaac381fd7cc09db0dbf1c63e4efc354ac
renedonaire <rene.donaire@gmail.com>
2022-05-09 22:24:55 -0400

    Argega api faker.js

---
+++ api/fakerApi.js
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker'
+faker.setLocale('es')
+
+const unProducto = async () => {
+	const product = await faker.commerce.product()
+	const price = await faker.commerce.price()
+	const image = await faker.image.imageUrl(640, 640, product)
+	return { product, price, image }
+}
+
+const variosProductos = async (cantidad) => {
+	let resultado = []
+	for (let i = 1; i <= cantidad; i++) {
+		resultado.push(await unProducto())
+	}
+	return resultado
+}
+
+module.exports = { variosProductos }
