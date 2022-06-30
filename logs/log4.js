299b8fe77f5de692a736eaee9fa8d3e29f137e32
renedonaire <rene.donaire@gmail.com>
2022-06-30 01:21:06 -0400

    Recupera archivos de log

---
+++ logs/log4.js
@@ -0,0 +1,27 @@
+const log4js = require('log4js')
+
+log4js.configure({
+	appenders: {
+		miLoggerConsole: { type: 'console' },
+		miLoggerWarning: { type: 'file', filename: 'logs/warning.log' },
+		miLoggerError: { type: 'file', filename: 'logs/error.log' },
+	},
+	categories: {
+		default: { appenders: ['miLoggerConsole'], level: 'trace' },
+		consola: { appenders: ['miLoggerConsole'], level: 'all' },
+		archivoWarning: {
+			appenders: ['miLoggerConsole', 'miLoggerWarning'],
+			level: 'warn',
+		},
+		archivoError: {
+			appenders: ['miLoggerConsole', 'miLoggerError'],
+			level: 'error',
+		},
+	},
+})
+
+const loggerConsola = log4js.getLogger('consola')
+const loggerWarning = log4js.getLogger('archivoWarning')
+const loggerError = log4js.getLogger('archivoError')
+
+module.exports = { loggerConsola, loggerWarning, loggerError }
